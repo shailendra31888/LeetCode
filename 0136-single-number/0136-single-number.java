80bdb251f3692d78492a5249ecbba9d5a26ea859
shailendra31888 <122545854+shailendra31888@users.noreply.github.com>
2024-12-22 15:03:23 +0530

    Time: 1 ms (99.87%), Space: 46.6 MB (11.02%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,29 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n= nums.length;
-        if(nums.length==1){
-            return nums[0];
+        //size of the array:
+        int n = nums.length;
+
+        // XOR all the elements:
+        int xorr = 0;
+        for (int i = 0; i < n; i++) {
+            xorr = xorr ^ nums[i];
         }
-        else{
-            for(int i=0;i<nums.length;i++){
-                for(int j=i+1;j<nums.length;j++){
-                    if(nums[i]==nums[j]){
-		            nums[i]=n+1;
-		            nums[j]=n+1;
-		        }
-		        else{
-		            continue;
-		        }
-                   
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-		    if(nums[i]!=n+1){
-		        return nums[i];
-		    }
-		}
-        return 0;
-        
+        return xorr;
     }
+
+   
 }
