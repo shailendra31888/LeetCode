60979ba0d272644e2a5a5ce84ef91e373c1745aa
shailendra31888 <122545854+shailendra31888@users.noreply.github.com>
2025-01-24 12:37:16 +0530

    Time: 1 ms (83.5%), Space: 41.6 MB (92.45%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,35 +1,38 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String ans ="";
+        StringBuilder ans =new StringBuilder("");
         int n = word1.length();
         int m = word2.length();
         if(n==m){
             for(int i=0;i<n;i++){
-                ans = ans +word1.charAt(i)+word2.charAt(i);
+            ans.append(word1.charAt(i)); // Append character from word1
+            ans.append(word2.charAt(i));
             }
         }
         else{
             if(n>m){
                 for(int i=0;i<m;i++){
-                ans = ans+ word1.charAt(i)+word2.charAt(i);
+                ans.append(word1.charAt(i)); // Append character from word1
+                ans.append(word2.charAt(i));
                 }
                 for(int i=m;i<n;i++){
-                ans = ans+word1.charAt(i);
+                ans.append(word1.charAt(i));
                 }
             }
 
             
             else{
                 for(int i=0;i<n;i++){
-                ans = ans+word1.charAt(i)+word2.charAt(i);
+                ans.append(word1.charAt(i)); // Append character from word1
+                ans.append(word2.charAt(i));
                 }
                 for(int i=n;i<m;i++){
-                ans = ans+word2.charAt(i);
+                ans.append(word2.charAt(i));
                 }
 
             }
         }
-        return ans;
+        return ans.toString();
         
     }
 }
