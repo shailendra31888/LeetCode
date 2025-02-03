26b9097c652cd8fd77c6ef435d3a5ce5e551306b
shailendra31888 <122545854+shailendra31888@users.noreply.github.com>
2025-02-03 15:17:33 +0530

    Time: 0 ms (100%), Space: 44.3 MB (70.62%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,33 +13,21 @@
  *     }
  * }
  */
- import java.util.*;
 class Solution {
+    private boolean result=true;
     public boolean isBalanced(TreeNode root) {
-        // If the result of the height function is -1, then the tree is not balanced.
-        return height(root) != -1;
+        validate(root);
+        return result;
+        
     }
-
-    public int height(TreeNode root) {
-        // Base case: if the node is null, its height is 0.
-        if (root == null) {
-            return 0;
+    public int validate(TreeNode root){
+        if(root==null) return 0;
+        int l=validate(root.left);
+        int r=validate(root.right);
+        if(Math.abs(l-r)>1){
+            result=false;
         }
+        return 1+Math.max(l,r);
 
-        // Recursively get the height of the left subtree.
-        int leftHeight = height(root.left);
-        if (leftHeight == -1) return -1; // Left subtree is not balanced
-
-        // Recursively get the height of the right subtree.
-        int rightHeight = height(root.right);
-        if (rightHeight == -1) return -1; // Right subtree is not balanced
-
-        // If the current node is not balanced, return -1.
-        if (Math.abs(leftHeight - rightHeight) > 1) {
-            return -1;
-        }
-
-        // Otherwise, return the height of the tree from this node.
-        return Math.max(leftHeight, rightHeight) + 1;
     }
 }
